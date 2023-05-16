ed8125f201f79e8284e4696adef3ebfcf2d751e5
yattoz <12408848+yattoz@users.noreply.github.com>
2023-05-16 17:37:47 +0200

    clone repo, install ruby deps, forward ports to host

--- run_qemu.bat
+++ run_qemu.bat
@@ -1,2 +1,7 @@
 :: QEMU is meant for testing, so we use a local web server to run the cloud-init config: 
-"C:\Program Files\qemu\qemu-system-x86_64.exe" -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22 -machine accel=whpx  -m 4096 -nographic -hda i.qcow2 -smbios type=1,serial="ds=nocloud-net;s=http://10.0.2.2:8000/"
+"C:\Program Files\qemu\qemu-system-x86_64.exe"^
+ -device e1000,netdev=net0 ^
+ -netdev user,id=net0,hostfwd=tcp::5555-:22,hostfwd=tcp::9999-:9090,hostfwd=tcp::8888-:80,hostfwd=tcp::4443-:443 ^
+ -machine accel=whpx  -m 4096 -nographic ^
+ -hda i.qcow2 ^
+ -smbios type=1,serial="ds=nocloud-net;s=http://10.0.2.2:8000/"
