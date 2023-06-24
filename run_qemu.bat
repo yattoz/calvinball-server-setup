4740e6f6f472894d14e8044abce58856b1acbe99
yattoz <12408848+yattoz@users.noreply.github.com>
2023-06-24 14:08:10 +0100

    added ports for nextcloud AIO interface check

--- run_qemu.bat
+++ run_qemu.bat
@@ -1,6 +1,7 @@
 
 :: Download image if not on disk yet
 if not exist dg.qcow2 (
+    :: curl -L -C - https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2 -o dg.qcow2
     curl -L -C - https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2 -o dg.qcow2
 )
 
@@ -14,9 +15,15 @@ if not exist i.qcow2 (
 )
 
 :: QEMU is meant for testing, so we use a local web server to run the cloud-init config: 
+:: service host port -> vm port
+:: cockpit 9999 -> 9090
+:: http 8888 -> 80
+:: https 4443 -> 443
+:: hugo  11313 -> 1313
+:: nexcloud-aio 38080 -> 8080
 "C:\Program Files\qemu\qemu-system-x86_64.exe"^
  -device e1000,netdev=net0 ^
- -netdev user,id=net0,hostfwd=tcp::5555-:22,hostfwd=tcp::9999-:9090,hostfwd=tcp::8888-:80,hostfwd=tcp::4443-:443,hostfwd=tcp::11313-:1313 ^
+ -netdev user,id=net0,hostfwd=tcp::5555-:22,hostfwd=tcp::9999-:9090,hostfwd=tcp::8888-:80,hostfwd=tcp::4443-:443,hostfwd=tcp::11313-:1313,hostfwd=tcp::38080-:8080 ^
  -machine accel=whpx -smp 12 -m 8192 -nographic ^
  -hda i.qcow2 ^
  -smbios type=1,serial="ds=nocloud-net;s=http://10.0.2.2:8000/"
